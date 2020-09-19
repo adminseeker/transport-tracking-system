056e745e6093b1fbdf2f14528bf07b48cefacf9f
Aravind A <aravind.arali@gmail.com>
2020-09-19 23:24:45 +0530

    added update operation for the vehicles table

--- backend/src/routers/api/vehicles.js
+++ backend/src/routers/api/vehicles.js
@@ -53,8 +53,38 @@ router.get("/",auth,async (req,res)=>{
     }
 });
 
-
-
+router.patch("/update/:id",auth,async (req,res)=>{
+    try {
+        if(req.user.isUpdater===1){
+            await mysql.query("SELECT * FROM vehicles JOIN updaters ON vehicles.id=updaters.vehicle_id WHERE vehicles.id = ? AND updaters.user_id = ?;",[req.params.id,req.user.id],async (error,results,fields)=>{
+                const vehicleString = JSON.stringify(results);
+                const vehicle = JSON.parse(vehicleString);
+                if(vehicle.length===0){
+                    res.json({msg:"no vehicle found"});
+                }
+                else{
+                    if(req.body.vehicle_number){
+                        await mysql.query("SELECT vehicle_number FROM vehicles WHERE vehicle_number = ?",[req.body.vehicle_number],async (error,results,fields)=>{
+                            if(error) throw error;
+                            if( results.length!==0 && results[0].vehical_number===vehicle.vehical_number){
+                                 res.status(400).json({msg:"This vehicle number already exists!"});
+                            }else{
+                                await mysql.query("UPDATE vehicles SET ? WHERE vehicles.id = ?",[req.body,req.params.id],(error,results,fields)=>{
+                                    res.json({msg:"update successfull!"});
+                                });
+                            }
+                        }); 
+                    }
+                }
+            });
+        }else{
+            res.json({msg:"Authorization Error!"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
 
 module.exports = router;
 
